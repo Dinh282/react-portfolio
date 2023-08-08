6c47314f17403be1c9a175dd81a60f9f7b69d1e2
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-08-08 10:17:58 -0400

    edit main.jsx to allow user to see about page upon initial visit to page. clean up code. remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
-
-import { BrowserRouter} from "react-router-dom"
-import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-// import Navbar from './components/Navbar.jsx';
-// import Footer from './Footer';
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Portfolio from './pages/Portfolio'
 import Resume from './pages/Resume'
 import Error from './pages/Error';
 
-
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/' ,
     element: <App />,
     errorElement: <Error />,
     children: [
       {
-        path: '/About', 
+        path: '/',
+        element: <About />,
+      },
+      {
+        path: '/About',
         element: <About />,
       },
       {
