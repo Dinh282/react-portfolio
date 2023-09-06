b45027d6f385011c4e95e86f9138a67561a208c9
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-09-06 08:19:26 -0400

    edit Dark and Light icons colors, removed hooks folder and Navbar.module.css, edit Navbar.jsx to show button in smaller screen size, update linkedIn link in footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,12 @@ import backgroundImage from "/assets/images/bg-img.jpg";
 import { useTheme } from "../../context/ThemeContext";
 import DarkIcon from "../Icons/DarkIcon/DarkIcon";
 import LightIcon from "../Icons/LightIcon/LightIcon";
-import useDarkModeStyles from "../../hooks/useDarkModeStyles";
-import styles from './Navbar.module.css'
+
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 export default function Navbar() {
   const currentPage = useLocation().pathname;
   const [darkMode, setDarkMode] = useTheme();
-  const adjustedStyles = useDarkModeStyles(styles);
 
   const navbarStyle = {
     backgroundImage: `url(${backgroundImage})`,
@@ -29,19 +27,18 @@ export default function Navbar() {
               </h1>
             </Link>
           </div>
+
+          <button
+            className="hover:scale-110"
+            onClick={() => {
+              setDarkMode(!darkMode);
+            }}
+          >
+            {darkMode ? <LightIcon /> : <DarkIcon />}
+          </button>
+
           <div className="flex-none nav-list ">
-            <ul className="menu menu-horizontal text-xl text-white p-6 font-bold tracking-wide">
-              <li>
-             <button
-              type='text'
-              onClick={() => {
-                setDarkMode(!darkMode);
-              }}
-              className={adjustedStyles.themeBtn}
-              >
-              {darkMode ? <LightIcon /> : <DarkIcon />}
-             </button>
-              </li>
+            <ul className="menu menu-horizontal text-xl text-white p-4 font-bold tracking-wide">
               <li>
                 <Link
                   to="/about"
@@ -87,73 +84,79 @@ export default function Navbar() {
             </ul>
           </div>
 
-          <div className="navbar-end">
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost btn-circle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-10 w-10"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="white"
+          <div>
+            <div className="navbar-end mx-4">
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-circle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-10 w-10"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="white"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h7"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-42 right-0"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h7"
-                  />
-                </svg>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-42 right-0"
-              >
-                <li>
-                  <Link
-                    to="/about"
-                    className={
-                      currentPage === "/about" ? "nav-link active" : "nav-link"
-                    }
-                  >
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/portfolio"
-                    className={
-                      currentPage === "/portfolio"
-                        ? "nav-link active"
-                        : "nav-link"
-                    }
-                  >
-                    Portfolio
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/contact"
-                    className={
-                      currentPage === "/contact"
-                        ? "nav-link active"
-                        : "nav-link"
-                    }
-                  >
-                    Contact
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/resume"
-                    className={
-                      currentPage === "/resume" ? "nav-link active" : "nav-link"
-                    }
-                  >
-                    Resume
-                  </Link>
-                </li>
-              </ul>
+                  <li>
+                    <Link
+                      to="/about"
+                      className={
+                        currentPage === "/about"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/portfolio"
+                      className={
+                        currentPage === "/portfolio"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
+                    >
+                      Portfolio
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/contact"
+                      className={
+                        currentPage === "/contact"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/resume"
+                      className={
+                        currentPage === "/resume"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
+                    >
+                      Resume
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </nav>
--- src/components/Navbar/index.js
+++
@@ -1,3 +0,0 @@
-import Navbar from ".";
-
-export default Navbar;
