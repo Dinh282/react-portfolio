b4e065a26dcb07ae94a89be41b613bef7d99efa4
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-09-13 08:40:23 -0400

    add summary to each project

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -11,7 +11,7 @@ const projects = [
     imageUrl: "/assets/images/dear-diary.jpg",
     liveUrl: "https://personal-journal-e23df6a117fd.herokuapp.com/",
     githubUrl: "https://github.com/Dinh282/personal-journal.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "Simple journal/diary application that allows users to record what is on their minds. User's entries are kept safe within their account.",
   },
   {
     title: "Sports Companion",
@@ -19,7 +19,7 @@ const projects = [
     imageUrl: "/assets/images/sports-companion.png",
     liveUrl: "https://dinh282.github.io/sports-companion/",
     githubUrl: "https://github.com/Dinh282/sports-companion.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "A compact application for referencing NBA team rosters and displaying information on the coach and players.",
   },
   {
     title: "Weather Dashboard",
@@ -27,7 +27,7 @@ const projects = [
     imageUrl: "/assets/images/weather-dashboard.png",
     liveUrl:"https://example.https://dinh282.github.io/weather-dashboard//project2",
     githubUrl: "https://github.com/Dinh282/weather-dashboard.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "This simple front-end web application utilizes the OpenWeather API to allow the user the ability to look up the weather forcast of a specific city.",
   },
   {
     title: "Tech Blog",
@@ -35,7 +35,7 @@ const projects = [
     imageUrl: "/assets/images/tech-blog.png",
     liveUrl: "https://dn-tech-blog-91088954e7f1.herokuapp.com/",
     githubUrl: "https://github.com/Dinh282/tech-blog.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "Basic web application that allows the user create an account, publish posts, and leave comments on other users' posts. With this application, users can effortlessly share their thoughts, engage in discussions, and manage their blog content.",
   },
   {
     title: "PWA Text Editor",
@@ -43,7 +43,7 @@ const projects = [
     imageUrl: "/assets/images/pwa-text-editor.png",
     liveUrl: "https://still-anchorage-67038-17721ce39121.herokuapp.com/",
     githubUrl: "https://github.com/Dinh282/pwa-text-editor.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "A single-page Progressive Web Application that provides the user access to a text-editor within the browser. ",
   },
   {
     title: "Chore Buddy",
@@ -51,7 +51,7 @@ const projects = [
     imageUrl: "/assets/images/ss-landing-page.jpg",
     liveUrl: "https://chore-buddy-6e6d2559dc60.herokuapp.com/",
     githubUrl: "https://github.com/Dinh282/chore-buddy.git",
-    summary: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    summary: "Full-stack web application that allows parents to create chores, set the allowance for each chore, and assign them to their children. Parents can also create individual accounts for their children. With their own accounts, the children can keep track of their balances and list of chores.",
   },
 ];
 
